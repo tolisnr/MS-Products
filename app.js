b6184c4fe3d20849b8cd9de7fcff1f4ad34f8bc1
Apostolos <siniorisap@yahoo.com>
2020-09-04 17:38:50 +0300

    Commit

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 
 //Start here
 
-const typed = new Typed('#typed', {
+/*const typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
     backSpeed: 85,
     typeSpeed: 60,
     loop: true
-  });
+  });*/
