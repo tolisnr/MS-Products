8edc10a48108d4d9b8573acdf51159ef107d0ba5
Apostolos <siniorisap@yahoo.com>
2020-10-11 17:56:09 +0300

    Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//Format document with Shift + Alt + F
+
+//Start here
+
+const typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    backSpeed: 85,
+    typeSpeed: 60,
+    loop: true,
+    backDelay: 500,
+    startDelay: 1000,
+    smartBackspace: true
+  });
